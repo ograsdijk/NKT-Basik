0261d4534292ab785fb4c413b3dac5113c6adad3
ograsdijk <o.grasdijk@gmail.com>
2021-02-02 17:14:10 -0500

    updated find device functions to properly close ports and devices after search

--- NKTBasik/functions.py
+++ NKTBasik/functions.py
@@ -1,15 +1,16 @@
 import logging
-from .device import Basik
+from .module import Basik
 from .dll.NKTP_DLL import DeviceResultTypes, openPorts, getAllPorts, \
                     deviceGetAllTypes, getOpenPorts, closePorts, deviceRemove
 
 
-def find_device_by_name(name):
+def find_device_by_name(name, ports = None):
     """Find Basik module com port and device id by checking the user modifiable
     text field
 
     Args:
-        name (str): device
+        name (str)      : device
+        ports (list)    : list port COM ports to look at, if None tries all
 
     Returns:
         tuple: (com, devID) or None if no device that matches found
@@ -18,7 +19,10 @@ def find_device_by_name(name):
     # automode: 0 open port, 1 open and start scanning devIDs
     # livemode: 0 disables continuous monitoring, 1 enable; allows for callbacks
     # takes about 2 to 3 s
-    openPorts(getAllPorts(), 1, 0)
+    if not ports:
+        openPorts(getAllPorts(), 1, 0)
+    else:
+        openPorts(','.join(ports),1,0)
 
     devices = {}
     for port in getOpenPorts().split(','):
@@ -30,25 +34,28 @@ def find_device_by_name(name):
         else:
             devices[port] = [devID for devID in range(len(device_types)) if device_types[devID] != 0]
 
+    closePorts(getOpenPorts())
+
     for com, devIDs in devices.items():
         for devID in devIDs:
             basik = Basik(com, devID)
             if basik.getName() == name:
-                deviceRemove(com, devID)
+                basik.close()
                 closePorts(getOpenPorts())
                 return com, devID
             else:
-                deviceRemove(com, devID)
+                basik.close()
 
     closePorts(getOpenPorts())
     return None
 
-def find_devices_by_names(names):
+def find_devices_by_names(names, ports = None):
     """Find Basik module com ports and device ids by checking the user 
     modifiable text field for each of the supplied names
 
     Args:
         names (list): list with devices names
+        ports (list): list port COM ports to look at, if None tries all
 
     Returns:
         dict: dictionary with a (com, devID) tuple for each name, or None if 
@@ -58,7 +65,10 @@ def find_devices_by_names(names):
     # automode: 0 open port, 1 open and start scanning devIDs
     # livemode: 0 disables continuous monitoring, 1 enable; allows for callbacks
     # takes about 2 to 3 s
-    openPorts(getAllPorts(), 1, 0)
+    if not ports:
+        openPorts(getAllPorts(), 1, 0)
+    else:
+        openPorts(','.join(ports),1,0)
 
     devices = {}
     for port in getOpenPorts().split(','):
@@ -70,13 +80,15 @@ def find_devices_by_names(names):
         else:
             devices[port] = [devID for devID in range(len(device_types)) if device_types[devID] != 0]
     
+    closePorts(getOpenPorts())
+
     devices_by_name = {name: None for name in names}
     for com, devIDs in devices.items():
         for devID in devIDs:
             basik = Basik(com, devID)
             if basik.getName() in names:
                 devices_by_name[basik.getName()] = (com, devID)
-            deviceRemove(com, devID)
+            basik.close()
 
     closePorts(getOpenPorts())
     return devices_by_name
